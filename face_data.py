26822dea757cc4ae02607eb4983c947b10fe2191
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-06-25 16:07:57 -0500

    face data

--- face_data.py
+++ face_data.py
@@ -11,4 +11,30 @@ dataset_path = "./face_dataset/"
 file_name = input("Enter the name of person : ")
 
 while True:
-    
+    ret, frame = cap.read()
+
+    gray_frame = cv2.cvtColor(frame.cv2.COLOR_BGR2GRAY) 
+
+    if ret == False:
+        continue
+
+    faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
+    if len(faces) == 0:
+        continue
+
+    k = 1
+
+    faces = sorted(faces, key = lambda x : x[2]*x[3], reverse = True)
+
+    skip += 1
+
+    for face in faces[:1]:
+        x,y,w,h = face
+
+        offset = 5
+        face_offset = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_selection = cv2.resize[face_offset,(100,100)]
+
+        if skip % 10 == 0:
+            face_data.append(face_selection)
+            print(len(face_data))
